2f3d204ab40ed9d19b0c3cab8326e2ba86e891c6
unknown <manurung0071@gmail.com>
2020-09-16 13:16:28 +0700

    AngelinaPanjaitan.java

---
+++ tugas/AngelinaPanjaitan.java
@@ -0,0 +1,9 @@
+public class AngelinaPanjaitan {
+	
+	public static void main(String[] args) {
+		System.out.println("Hallo ban");
+		System.out.println("Maaf mengganggumu");
+		System.out.println("Cuma mau bilang aku akan sukses nanti");
+		System.out.println("Doain ya");
+	}
+}
